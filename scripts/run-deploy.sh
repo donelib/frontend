0de30ee583f9230d1f1a0ebecc11ddf117b34b1c
skdlsco <nicnic4102@gmail.com>
2023-02-09 21:56:52 +0900

    [ADD] github action + aws code deploy

---
+++ scripts/run-deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+WORK_DIR=/home/ec2-user/build/front
+BUILD_DIR=$WORK_DIR/build
+RELEASE_PATH=$WORK_DIR/release
+
+BUILD_NAME=$(ls $BUILD_DIR | grep 'build_' | tail -n 1)
+BACKUP_PATH=$WORK_DIR/backup/$BUILD_NAME
+
+echo "> cd work dir $WORK_DIR"
+cd $WORK_DIR
+
+echo "> copy build file to $BACKUP_PATH"
+cp build/$BUILD_NAME $BACKUP_PATH
+
+echo "> link"
+ln -sfn $BUILD_NAME $RELEASE_PATH
+exit $?
