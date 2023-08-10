5b0f9c7a539274349c7d00138cbdb65da48d9e83
skdlsco <nicnic4102@gmail.com>
2023-08-10 12:45:00 +0900

    [ADD] analyze 현재 날짜 기준으로 출력

--- src/page/analyze/Analyze.tsx
+++ src/page/analyze/Analyze.tsx
@@ -6,44 +6,53 @@ import DoneBarGraph from "./../../component/Analyze/DoneBarGraph/DoneBarGraph";
 import DoneCalendarGraph from "../../component/Analyze/DoneCalendarGraph/DoneCalendarGraph";
 
 const Root = styled.div`
-  display: flex;
-  flex-direction: column;
+	display: flex;
+	flex-direction: column;
 `;
 
 const Container = styled(Box)`
-  display: flex;
-  flex-direction: column;
-  min-width: 600px;
-  margin: 0 auto;
-  padding: 0px 24px;
-  @media (max-width: 700px) {
-    min-width: 0px;
-    margin: 0;
-  }
+	display: flex;
+	flex-direction: column;
+	min-width: 600px;
+	margin: 0 auto;
+	padding: 0px 24px;
+	@media (max-width: 700px) {
+		min-width: 0px;
+		margin: 0;
+	}
 `;
 
 const Title = styled.h2``;
 const SubTitle = styled.h3`
-  margin: 18px 0 12px 0;
+	margin: 18px 0 12px 0;
 `;
 
-const Analyze = () => {
-  const [date] = useState(Date.now());
+const getMonthFromDatetime = (dt: number) => {
+	return String(new Date(dt).getMonth() + 1).padStart(2, "0");
+};
 
-  return (
-    <Root>
-      <DefaultAppBar />
-      <Container>
-        <Title>Analyze</Title>
-        <SubTitle>태그별 done 개수</SubTitle>
-        <div>2023-03</div>
-        <DoneBarGraph date={new Date(date)} />
-        <SubTitle>날짜별 done 개수</SubTitle>
-        <div>2023년</div>
-        <DoneCalendarGraph date={new Date(date)} />
-      </Container>
-    </Root>
-  );
+const getFullYearFromDatetime = (dt: number) => {
+	return new Date(dt).getFullYear();
+};
+
+const Analyze = () => {
+	const [date] = useState(Date.now());
+	return (
+		<Root>
+			<DefaultAppBar />
+			<Container>
+				<Title>Analyze</Title>
+				<SubTitle>태그별 done 개수</SubTitle>
+				<div>
+					{getFullYearFromDatetime(date)}-{getMonthFromDatetime(date)}
+				</div>
+				<DoneBarGraph date={new Date(date)} />
+				<SubTitle>날짜별 done 개수</SubTitle>
+				<div>{getFullYearFromDatetime(date)}</div>
+				<DoneCalendarGraph date={new Date(date)} />
+			</Container>
+		</Root>
+	);
 };
 
 export default Analyze;
